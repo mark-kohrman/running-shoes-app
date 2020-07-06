1c976823a25b78eaa02582640c1f3bc9de21d696
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-05 21:29:08 -0500

    adding index and show actions

--- app/controllers/api/shoes_controller.rb
+++ app/controllers/api/shoes_controller.rb
@@ -1,5 +1,12 @@
 class Api::ShoesController < ApplicationController
   def index
+    @shoes = Shoe.all
     render 'index.json.jb'
   end
+
+  def show
+    @shoe = Shoe.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     get "/shoes" => "shoes#index"
   end
+
+  namespace :api do
+    get "/shoes/:id" => "shoes#show"
+  end
+  
 end
